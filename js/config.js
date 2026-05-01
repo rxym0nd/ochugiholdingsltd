d53895f74a499aa0c6ce48c1445a239b6e51fc46
Raymond <rxym0nd09@gmail.com>
2026-05-01 12:08:58 +0300

    Update API URL to live Render server

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   // Local development URL. When you deploy the backend to Render, 
   // change this to your live URL (e.g., 'https://ochugi-backend.onrender.com')
-  API_URL: 'http://localhost:3000'
+  API_URL: 'https://ochugiholdingsltd.onrender.com'
 };
